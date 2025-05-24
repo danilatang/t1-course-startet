9958364918f89a5acdb97764cff7625cd5efecb9
danilatang <d6nil9@gmail.com>
2025-05-24 11:24:13 +0300

    task/#3-logging-starter#3 - changes made

--- src/main/java/com/example/config/HttpLoggingAutoConfiguration.java
+++ src/main/java/com/example/config/HttpLoggingAutoConfiguration.java
@@ -2,12 +2,18 @@ package com.example.config;
 
 import com.example.aspect.HttpLoggingAspect;
 import com.example.properties.HttpLoggingProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(HttpLoggingProperties.class)
+@ConditionalOnProperty(
+        prefix = "logging.from.starter",
+        name = "enabled",
+        havingValue = "true"
+)
 public class HttpLoggingAutoConfiguration {
 
     @Bean
--- src/main/java/com/example/properties/HttpLoggingProperties.java
+++ src/main/java/com/example/properties/HttpLoggingProperties.java
@@ -5,16 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "logging.from.starter")
 public class HttpLoggingProperties {
-    private boolean enabled;
     private LogLevel logLevel = LogLevel.INFO;
-
-    public boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
 
     public LogLevel getLevel() {
         return logLevel;
